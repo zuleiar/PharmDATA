62cbaf682d081838043475bcc403e2afe9e01aa5
Zuleika Aguilar Roldan <zuleika.aguilar@lasallistas.org.mx>
2026-05-02 20:59:15 -0600

    Add files via upload

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2721636107" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4035747128" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
